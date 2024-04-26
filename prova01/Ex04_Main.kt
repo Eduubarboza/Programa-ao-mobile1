ef47f3a3d97a131b7518437557dd99e146591265
Eduubarboza <161897415+Eduubarboza@users.noreply.github.com>
2024-04-26 19:38:27 -0300

    Update Ex04_Main.kt

--- prova01/Ex04_Main.kt
+++ prova01/Ex04_Main.kt
@@ -1,3 +1,4 @@
+//Nome: João Vitor de Assis Martins e Eduardo Batista Barboza
 /*Crie uma classe Carta que represente uma carta de baralho com atributos como
 naipe e valor. Crie uma classe Baralhoque represente um baralho de cartas completo.
 Implemente métodos para embaralhar o baralho, distribuir cartas. Entregue 5 cartas
