00e805fd5fe52061f760129f21b22c84803ec226
Eduubarboza <161897415+Eduubarboza@users.noreply.github.com>
2024-04-26 19:36:23 -0300

    Update Ex03_Animal.kt

--- prova01/Ex03_Animal.kt
+++ prova01/Ex03_Animal.kt
@@ -1,3 +1,4 @@
+//Nome: João Vitor de Assis Martins e Eduardo Batista Barboza
 abstract class Animal(open val nome: String, open val idade: Int, open val som: String
                         ,open val Sexo: String)
 {
