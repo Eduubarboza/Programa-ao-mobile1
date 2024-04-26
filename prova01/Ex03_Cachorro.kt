6e0a7ffee03f145987737e8ec8aa8a917fe451c8
Eduubarboza <161897415+Eduubarboza@users.noreply.github.com>
2024-04-26 19:36:43 -0300

    Update Ex03_Cachorro.kt

--- prova01/Ex03_Cachorro.kt
+++ prova01/Ex03_Cachorro.kt
@@ -1,3 +1,4 @@
+//Nome: João Vitor de Assis Martins e Eduardo Batista Barboza
 class Cachorro(nome: String, idade: Int, Sexo: String) : Animal(nome, idade, "Rhhhaarrrr", Sexo) {
     // Implementação do método fazerSom para o pássaro
     override fun fazerSom() {
